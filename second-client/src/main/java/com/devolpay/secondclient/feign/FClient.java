5be86f07b3bdc991580b5f0951851171365f457c
Nat Hidalgo <118085025+neilnathanhidalgo@users.noreply.github.com>
2023-03-09 10:37:16 -0500

    mejora en Circuit breaker

--- second-client/src/main/java/com/devolpay/secondclient/feign/FClient.java
+++ second-client/src/main/java/com/devolpay/secondclient/feign/FClient.java
@@ -1,8 +1,6 @@
 package com.devolpay.secondclient.feign;
 
 import feign.FeignException;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
